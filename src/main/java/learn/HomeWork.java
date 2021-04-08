09291f085e6306e795ed6db2b60b075190d29444
aviadamd <aviadamd@gmail.com>
2021-04-08 22:42:20 +0300

    add files and tests

---
+++ src/main/java/learn/HomeWork.java
@@ -0,0 +1,51 @@
+package learn;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class HomeWork {
+
+    private HomeWorkComponents homeWorkComponents;
+
+    @BeforeEach
+    public void initHomeWorkComponents() {
+        homeWorkComponents = new HomeWorkComponents();
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("question 1: printValuesBetween2VariablesWithoutUsingThirdVariable")
+    public void printValuesBetween2VariablesWithoutUsingThirdVariable() {
+        homeWorkComponents.changeValuesBetween2VariablesWithoutUsingThirdVariable(1,1,1,1);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("question 2: recursionMethodTest")
+    void recursionMethodTest() {
+        System.out.println(homeWorkComponents.recursionMethod(3));
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("question 3: sortedHighestNumberFromList")
+    void sortedHighestNumberFromList() {
+        System.out.println(homeWorkComponents
+                .sortNumbersFromLowestNumberToHighest(homeWorkComponents.numbers));
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("question 3: sortedHighestNumberFromList")
+    void printTheLowestAndTheHighestNumbers() {
+        homeWorkComponents.printTheLowestAndTheHighest(homeWorkComponents.numbers);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("question 4: printRandomNumbersWithoutRepetition")
+    public void printRandomNumbersWithoutRepetition() {
+        homeWorkComponents.randomNumbersWithoutRepetition(1000).forEach(System.out::println);
+    }
+
+}
---
+++ src/main/java/learn/HomeWorkComponents.java
@@ -0,0 +1,76 @@
+package learn;
+
+import java.util.*;
+
+public class HomeWorkComponents {
+
+    protected final List<Integer> numbers = new ArrayList<>(Arrays.asList(6,11,4,15,1000,36,7));
+
+    protected void changeValuesBetween2VariablesWithoutUsingThirdVariable(int a, int b, int x, int y) {
+        if (a == x && b == y) {
+
+            a = x + b;
+            b = x - a;
+            a = x - y;
+
+            System.out.println("a value : " + a);
+            System.out.println("b value : " + b);
+            System.out.println("x value : " + x);
+            System.out.println("y value : " + y);
+        }
+    }
+
+    protected int recursionMethod(int n) {
+        if (n <= 1)
+            return 1;
+        return (n * recursionMethod(n -1));
+    }
+
+    protected List<Integer> sortNumbersFromLowestNumberToHighest(List<Integer> numbers) {
+        Collections.sort(numbers);
+        Collections.reverse(numbers);
+        return numbers;
+    }
+
+    protected void printTheLowestAndTheHighest(List<Integer> numbers) {
+        int largest = numbers.get(0);
+        int smallest = numbers.get(0);
+
+        for (Integer number : numbers) {
+            if (number > largest) {
+                largest = number;
+            }
+            if (number < smallest) {
+                smallest = number;
+            }
+        }
+
+        System.out.println("Largest number :" + largest);
+        System.out.println("Smallest number :" + smallest);
+    }
+
+    protected List<Integer> randomNumbersWithoutRepetition(int N) {
+        //array to store N random integers (0 - N-1)
+        int[] nums = new int[N];
+
+        // initialize each value at index i to the value i
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = i;
+        }
+
+        Random randomGenerator = new Random(); // using the random class util
+        int randomIndex; // the randomly selected index each time through the loop
+        int randomValue; // the value at nums[randomIndex] each time through the loop
+        List<Integer> collector = new ArrayList<>();
+        // randomize order of values
+        for(int i = 0; i < nums.length; ++i) {
+            randomIndex = randomGenerator.nextInt(nums.length);  // select a random index
+            randomValue = nums[randomIndex]; //swap values and hold all the numbers within the random index int
+            nums[randomIndex] = nums[i]; // start i from the first num 0
+            nums[i] = randomValue; // hold i inside numbs array start from num 0
+            collector.add(nums[i]); // add the num value to collector list
+        }
+
+        return collector; // return fix random list
+    }
+}
